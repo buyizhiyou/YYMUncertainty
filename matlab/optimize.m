4a1902e7ada039b85f7958dfbcba6658b4551b3e
shiqing <2557040812@qq.com>
2024-01-22 21:11:14 +0800

    add new papers

--- matlab/optimize.m
+++
@@ -1,6 +0,0 @@
-load("optimize.mat")
-options = optimoptions('quadprog','Display','iter');
-lb = [];
-ub = [];
-[x fval,exitflag,output] = quadprog(H,f,A,b,Aeq,beq,lb,ub,x0,options);
-[argvalue, argmax] = max(x);
